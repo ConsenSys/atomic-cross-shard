9d50461d289c2c1ebb73d0ff1163654b1eb04bad
Peter  Robinson <drinkcoffee@eml.cc>
2020-03-27 10:05:39 +1000

    Initial versions
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

---
+++ sim/src/main/java/tech/pegasys/teamx/crossshardsim/CrossShardSimMain.java
@@ -0,0 +1,9 @@
+package tech.pegasys.teamx.crossshardsim;
+
+public class CrossShardSimMain {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("Here");
+  }
+
+}
